4c127ac39fc8ee0e0e4f9de61fdbfee03c681677
LDecker <Decker702@yahoo.com>
2017-04-24 16:55:55 -0400

    Create API and put in plumbing

--- ElevenNote.Data/IdentityModels.cs
+++ ElevenNote.Data/IdentityModels.cs
@@ -18,51 +18,64 @@ namespace ElevenNote.Data
             // Add custom user claims here
             return userIdentity;
         }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+        // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+        //  public class ApplicationUser : IdentityUser
+        //  {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            // Add custom user claims here
+            return userIdentity;
         }
 
-        public static ApplicationDbContext Create()
+
+        //  }
+
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
         {
-            return new ApplicationDbContext();
+            public ApplicationDbContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            }
+
+            public static ApplicationDbContext Create()
+            {
+                return new ApplicationDbContext();
+            }
+
+            public DbSet<Note> Notes { get; set; }//Portal through services layer to the data base
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .Conventions
+                    .Remove<PluralizingTableNameConvention>();
+
+                modelBuilder
+                    .Configurations
+                    .Add(new IdentityUserLoginConfiguration())
+                    .Add(new IdentyUserRoleConfiguration());
+            }
         }
 
-        public DbSet<Note> Notes { get; set; }//Portal through services layer to the data base
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        public class IdentityUserLoginConfiguration
+            : EntityTypeConfiguration<IdentityUserLogin>
         {
-            modelBuilder
-                .Conventions
-                .Remove<PluralizingTableNameConvention>();
-
-            modelBuilder
-                .Configurations
-                .Add(new IdentityUserLoginConfiguration())
-                .Add(new IdentyUserRoleConfiguration());
-        }
-    }
-
-    public class IdentityUserLoginConfiguration
-        : EntityTypeConfiguration<IdentityUserLogin>
-    {
-        public IdentityUserLoginConfiguration()
-        {
-            HasKey(iul => iul.UserId);
-        }
-    }
-
-    public class IdentyUserRoleConfiguration
-        : EntityTypeConfiguration<IdentityUserRole>
-    {
-        public IdentyUserRoleConfiguration()
-        {
-            HasKey(iur => iur.RoleId);
+            public IdentityUserLoginConfiguration()
+            {
+                HasKey(iul => iul.UserId);
+            }
         }
 
+        public class IdentyUserRoleConfiguration
+            : EntityTypeConfiguration<IdentityUserRole>
+        {
+            public IdentyUserRoleConfiguration()
+            {
+                HasKey(iur => iur.RoleId);
+            }
+
+        }
     }
 }

46212c9ebe912e3ba3c55df8f2ed0933ecf0e82a
LDecker <Decker702@yahoo.com>
2017-04-25 12:12:16 -0400

    worked with Fiddler to log on

---
+++ ElevenNote.API/Controllers/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using ElevenNote.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace ElevenNote.API.Controllers
+{
+    [Authorize]
+    public class NotesController : ApiController
+    {
+        public IHttpActionResult GetAll()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            var notes = noteService.GetNotes();
+        
+            return Ok(notes);
+        }
+    }
+}
